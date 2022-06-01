bedd7f21a4f2e61b8b78d7d4398e95794316991f
KirPrim <90135321+KirPrim@users.noreply.github.com>
2022-06-01 13:58:44 +0300

    Create Priimakov_Kirill_task1.py

---
+++ Priimakov_Kirill_task1.py
@@ -0,0 +1,198 @@
+text1='Nyrk zj Trisrerb?'
+text2='Trisrerb zj kyv erdv nv ljv wfi re RGK-jkpcv trdgrzxe krixvkzex (slk efk czdzkvu kf) wzeretzrc zejkzklkzfej. Kyv drze uzwwvivetv nzky fkyvi RGK rkkrtbj zj kyrk rkkrtbvij uf efk jvv urkr slk dfevp rj kyvzi gizdrip krixvk. Nv jrp RGK-czbv, yfnvmvi kyv rkkrtb zj efk jkiztkcp jgvrbzex Rumretvu. Jkiztkcp jgvrbzex, kyv drze wvrkliv uvwzezex kyv rkkrtbvij zj Gvijzjkvetv Nv erdv kyv srtbuffi Trisrerb jzetv zk zj srjvu fe Trisvig reu kyv erdv fw kyv tfewzxlirkzfe wzcv zj “rerb.twx”.'
+text3='Epib izm bpm uitqkqwca xczxwama wn bpqa kiuxiqov?'
+text4='Bpm ibbiksmza qvnqtbzibm bpm dqkbqu´a vmbewzs twwsqvo nwz bpm kzqbqkit agabmu bpmg kiv cam nwz kiapqvo uwvmg wcb. Wvkm bpmg pidm abwtmv i aqovqnqkivb iuwcvb wn uwvmg (nzwu 2.5 bw 10 UU CAL xmz mvbqbg), bpmg ijivlwv bpm dqkbqu.'
+text5='Itk pa kag ftuzw uf ue euszuruomzf?'
+text6='Nmzwuzs qzfufuqe tmhq mximke nqqz m bduymdk fmdsqf rad oknqdoduyuzmxe. Taiqhqd uf ime mxyaef mximke ftdagst ftqud ogefayqde. Ftue fuyq mffmowqde mdq fmdsqfuzs ruzmzoumx qzfufuqe pudqofxk uz mz gzbdqoqpqzfqp, pqfqdyuzqp, tustxk bdarqeeuazmx mzp oaadpuzmfqp mffmow, mzp geuzs mzk yqmze rday ftq fmdsqf fa omet me ygot yazqk agf me baeeunxq, gb fa mz mbbmdqzfxk mgfa-uybaeqp xuyuf.'
+text7='Ger csy ibtpemr xli xmqipmri sj xli geqtemkr?'
+text8='Eggsvhmrk xs alex ai orsa, xli jmvwx qepmgmsyw weqtpiw aivi gsqtmpih mr Eykywx, 2013 alir xli gcfivgvmqmrepw wxevxih xs xiwx xli Gevfereo qepaevi. Xli jmvwx mrjigxmsrw aivi hixigxih mr Higiqfiv, 2013. Sr eziveki, iegl fero vsffivc xsso fixaiir xas erh jsyv qsrxlw, jvsq mrjigxmrk xli jmvwx gsqtyxiv ex xli fero’w gsvtsvexi rixasvo xs gewlmrk xli qsric syx. Ai fipmizi xlex xli kerk aew efpi xs wyggiwwjyppc wxiep jvsq xlimv jmvwx zmgxmqw hyvmrk xli tivmsh sj Jifvyevc-Etvmp 2014. Xli tieo sj mrjigxmsrw aew vigsvhih mr Nyri 2014. Gyvvirxpc xli geqtemkr mw wxmpp egxmzi.'
+text9='Dof kpku´a fvb thrl aol klahpsz wbispj buaps uvd?'
+text10='Zpujl dl zahyalk dvyrpun vu aopz jhtwhpnu dl ohcl jvsshivyhalk dpao aol kpmmlylua SLHz pucvsclk pu aol puclzapnhapvu huk olswlk aolt hz tbjo hz wvzzpisl. Hz pa ylthpuz hu vwlu puclzapnhapvu, dl dlyl hzrlk uva av zohyl huf klahpsz buaps pa dhz zhml av kv zv. Ohcl fvb ylhjolk cpjaptz huk Jvtwbaly Ltlynlujf Ylzwvuzl Alhtz (JLYAz) pu aovzl jvbuayplz dolyl fvb ohcl klaljalk aol pujpkluaz?'
+text11='Flz, aopz puclzapnhapvu abyulk puav h qvpua vwlyhapvu iladllu Rhzwlyzrf Shi’z Nsvihs Ylzlhyjo huk Huhsfzpz Alht huk pualyuhapvuhs vynhupghapvuz, uhapvuhs huk ylnpvuhs shd lumvyjltlua hnlujplz huk h ubtily vm Jvtwbaly Ltlynlujf Ylzwvuzl Alhtz (JLYAz) dvyskdpkl. Vul vm vby thpu nvhsz dhz av kpzzltpuhal vby ruvdslknl vm aol jhtwhpnu huk PVJz htvun hss klaljalk huk wvaluaphs cpjaptz. Dl bzlk uhapvuhs JLYAz huk SLHz hz aol kpzaypibapvu johuuls.'
+text12='Qxf mrm hxd lxwcarkdcn cx cqn rwenbcrpjcrxw?'
+text13='Fn’an qnuyrwp cx jbbrbc rw rwenbcrpjcrxwb jwm lxdwcnavnjbdanb cqjc mrbadyc vjufjan xynajcrxwb jwm lhknalarvrwju jlcrerch. Mdarwp cqn rwenbcrpjcrxwb fn yaxermn cnlqwrlju ngynacrbn bdlq jb jwjuhirwp rwonlcrxw enlcxab, vjurlrxdb yaxpajvb, bdyyxacnm Lxvvjwm & Lxwcaxu rwoajbcadlcdan jwm ngyuxrcjcrxw vncqxmb.'
+text14='Ubj jnf gur znyjner qvfgevohgrq?'
+text15='Nggnpxref hfrq fcrne cuvfuvat rznvyf jvgu znyvpvbhf nggnpuzragf ntnvafg rzcyblrrf bs gur gnetrgrq svanapvny vafgvghgvbaf, va fbzr pnfrf fraqvat gurz gb gurve crefbany rznvy nqqerffrf. Jr oryvrir gur nggnpxref nyfb hfrq qevir ol qbjaybnq nggnpxf, ohg guvf frpbaq nffhzcgvba vf fgvyy abg 100% pbasvezrq.'
+text16='Biq uly wiljiluny omylm jlinywnyx uauchmn nbcm nsjy iz unnuwe?'
+text17='Sym, qy xynywn Wulvuhue mugjfym um Vuwexiil.Qch32.Wulvuhue uhx Vuwexiil.Qch32.WulvuhueWgx. Uff Eumjylmes Fuv’m wiljiluny jlixownm uhx mifoncihm xynywn ehiqh Wulvuhue mugjfym. Ni lucmy nby fypyf iz jlinywncih, cn cm lywiggyhxyx ni mqcnwb ih Eumjylmes’m Jliuwncpy Xyzyhmy Gixofy chwfoxyx ch yuwb gixylh jlixown uhx mifoncih. Qy ufmi bupy migy ayhyluf lywiggyhxuncihm: Xi hin ijyh momjcwciom ygucfm, ymjywcuffs cz nbys bupy uh unnuwbgyhn; Ojxuny siol miznquly (ch nbcm wugjucah hi 0xusm qyly omyx); Nolh ih byolcmncwm ch siol mywolcns mocnym, nbcm qus cn cm gily fceyfs nbun mowb hyq mugjfym qcff vy xynywnyx uhx mnijjyx zlig nby vyachhcha. '
+
+
+alf = ''.join([chr(i) for i in range(ord('A'),ord('Z')+1)])
+alf2 = ''.join(list(map(chr, range(ord('a'), ord('z')+1))))
+key = 17
+key3=8
+key5=12
+key7=4
+key9=7
+key12=9
+key14=13
+key16=20
+b1 = ''
+b2=''
+b3=''
+b4=''
+b5=''
+b6=''
+b7=''
+b8=''
+b9=''
+b10=''
+b11=''
+b12=''
+b13=''
+b14=''
+b15=''
+b16=''
+b17=''
+for i in text1:
+    if i in alf:
+        b1 += alf[alf.find(i)-key]
+    elif i in alf2:
+        b1 += alf2[alf2.find(i)-key]
+    else:
+        b1+=i
+print(b1)
+
+for i in text2:
+    if i in alf:
+        b2 += alf[alf.find(i)-key]
+    elif i in alf2:
+        b2 += alf2[alf2.find(i)-key]
+    else:
+        b2+=i
+print(b2)
+
+for i in text3:
+    if i in alf:
+        b3 += alf[alf.find(i)-key3]
+    elif i in alf2:
+        b3 += alf2[alf2.find(i)-key3]
+    else:
+        b3+=i
+print(b3)
+
+for i in text4:
+    if i in alf:
+        b4 += alf[alf.find(i)-key3]
+    elif i in alf2:
+        b4 += alf2[alf2.find(i)-key3]
+    else:
+        b4+=i
+print(b4)
+
+for i in text5:
+    if i in alf:
+        b5 += alf[alf.find(i)-key5]
+    elif i in alf2:
+        b5 += alf2[alf2.find(i)-key5]
+    else:
+        b5+=i
+print(b5)
+
+for i in text6:
+    if i in alf:
+        b6 += alf[alf.find(i)-key5]
+    elif i in alf2:
+        b6 += alf2[alf2.find(i)-key5]
+    else:
+        b6+=i
+print(b6)
+
+for i in text7:
+    if i in alf:
+        b7 += alf[alf.find(i)-key7]
+    elif i in alf2:
+        b7 += alf2[alf2.find(i)-key7]
+    else:
+        b7+=i
+print(b7)
+
+for i in text8:
+    if i in alf:
+        b8 += alf[alf.find(i)-key7]
+    elif i in alf2:
+        b8 += alf2[alf2.find(i)-key7]
+    else:
+        b8+=i
+print(b8)
+
+for i in text9:
+    if i in alf:
+        b9 += alf[alf.find(i)-key9]
+    elif i in alf2:
+        b9 += alf2[alf2.find(i)-key9]
+    else:
+        b9+=i
+print(b9)
+
+for i in text10:
+    if i in alf:
+        b10 += alf[alf.find(i)-key9]
+    elif i in alf2:
+        b10 += alf2[alf2.find(i)-key9]
+    else:
+        b10+=i
+print(b10)
+
+for i in text11:
+    if i in alf:
+        b11 += alf[alf.find(i)-key9]
+    elif i in alf2:
+        b11 += alf2[alf2.find(i)-key9]
+    else:
+        b11+=i
+print(b11)
+
+for i in text12:
+    if i in alf:
+        b12 += alf[alf.find(i)-key12]
+    elif i in alf2:
+        b12 += alf2[alf2.find(i)-key12]
+    else:
+        b12+=i
+print(b12)
+
+for i in text13:
+    if i in alf:
+        b13 += alf[alf.find(i)-key12]
+    elif i in alf2:
+        b13 += alf2[alf2.find(i)-key12]
+    else:
+        b13+=i
+print(b13)
+
+for i in text14:
+    if i in alf:
+        b14 += alf[alf.find(i)-key14]
+    elif i in alf2:
+        b14 += alf2[alf2.find(i)-key14]
+    else:
+        b14+=i
+print(b14)
+
+for i in text15:
+    if i in alf:
+        b15 += alf[alf.find(i)-key14]
+    elif i in alf2:
+        b15 += alf2[alf2.find(i)-key14]
+    else:
+        b15+=i
+print(b15)
+
+for i in text16:
+    if i in alf:
+        b16 += alf[alf.find(i)-key16]
+    elif i in alf2:
+        b16 += alf2[alf2.find(i)-key16]
+    else:
+        b16+=i
+print(b16)
+
+for i in text17:
+    if i in alf:
+        b17 += alf[alf.find(i)-key16]
+    elif i in alf2:
+        b17 += alf2[alf2.find(i)-key16]
+    else:
+        b17+=i
+print(b17)
